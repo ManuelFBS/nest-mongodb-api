331e089632bd5df02de5114de1318c453ea80254
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-31 11:19:13 -0400

    Modificando app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://manuelfbs:321Shalom321@cluster0.irhtnet.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

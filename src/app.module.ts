ff9037f23f9738fc20c4ba9dce95138bf148ea0f
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-02 11:17:04 -0400

    3ra modif. app.module.ts, Creac. todo.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://manuelfbs:321Shalom321@cluster0.irhtnet.mongodb.net/nestjsdb?retryWrites=true&w=majority',
     ),
+    TodoModule,
   ],
   controllers: [AppController],
   providers: [AppService],

348f8f42603f4ad55da518f29a8aea704e29401f
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-04 12:18:27 -0400

    Modificacion de todo.service.ts

--- src/todo/services/todo.service.ts
+++ src/todo/services/todo.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Todo } from '../schemas/todo.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(@InjectModel(Todo.name) private todoModel: Model<Todo>) {}
+
+  public async create(todo: any) {
+    const createdTodo = new this.todoModel(todo);
+
+    return createdTodo.save();
+  }
+}

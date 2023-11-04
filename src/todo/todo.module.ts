0ea168a76eb41064359ec9468f44a9c99e6e3723
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-04 11:23:34 -0400

    Actualizacion todo.module.ts

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Todo, TodoSchema } from './schemas/todo.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Todo.name,
+        schema: TodoSchema,
+      },
+    ]),
+  ],
+})
 export class TodoModule {}

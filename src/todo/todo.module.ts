346ee63314e919beaa605df4a8609e7ec38a7684
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-04 11:52:08 -0400

    Creac. todo.service.ts, Act. todo.module.ts

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Todo, TodoSchema } from './schemas/todo.schema';
 import { TodoController } from './controllers/todo.controller';
+import { TodoService } from './services/todo.service';
 
 @Module({
   imports: [
@@ -13,5 +14,6 @@ import { TodoController } from './controllers/todo.controller';
     ]),
   ],
   controllers: [TodoController],
+  providers: [TodoService],
 })
 export class TodoModule {}

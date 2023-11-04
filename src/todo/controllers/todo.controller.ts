2e347daed54b6f4d0a649e09c7fef0fc1f26f6ff
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-04 13:32:50 -0400

    Modificacion de todo.controller.ts

--- src/todo/controllers/todo.controller.ts
+++ src/todo/controllers/todo.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { TodoService } from '../services/todo.service';
+import { CreateTodoDTO } from '../dto/create-todo.dto';
 
 @Controller('todo')
 export class TodoController {
   constructor(private todoService: TodoService) {}
 
   @Post('create')
-  public async create(@Body() createTodo: any) {
+  public async create(@Body() createTodo: CreateTodoDTO) {
     return this.todoService.create(createTodo);
   }
 }

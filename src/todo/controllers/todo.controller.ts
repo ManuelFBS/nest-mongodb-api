f7e5fac7639a04d416360d950b256dc864911711
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-05 03:05:36 -0400

    Actualizacion de todo.controller.ts

--- src/todo/controllers/todo.controller.ts
+++ src/todo/controllers/todo.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  ValidationPipe,
+} from '@nestjs/common';
 import { TodoService } from '../services/todo.service';
 import { CreateTodoDTO } from '../dto/create-todo.dto';
+import { UpdateTodoDTO } from '../dto/update-todo.dto';
 
 @Controller('todo')
 export class TodoController {
@@ -9,5 +19,28 @@ export class TodoController {
   @Post('create')
   public async create(@Body(new ValidationPipe()) createTodo: CreateTodoDTO) {
     return this.todoService.create(createTodo);
+  }
+
+  @Get('getall')
+  public async findAll() {
+    return this.todoService.findAll();
+  }
+
+  @Get('find/:id')
+  public async findOne(@Param('id') id: string) {
+    return this.todoService.findOne(id);
+  }
+
+  @Put('update/:id')
+  public async update(
+    @Param('id') id: string,
+    @Body(new ValidationPipe()) updateTodo: UpdateTodoDTO,
+  ) {
+    return this.todoService.update(id, updateTodo);
+  }
+
+  @Delete('del/:id')
+  public async delete(@Param('id') id: string) {
+    return this.todoService.delete(id);
   }
 }

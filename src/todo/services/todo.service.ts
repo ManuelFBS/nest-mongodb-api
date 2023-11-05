9108a89eed9ded2a6146eea4993dca99c2a66cb7
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-05 02:12:15 -0400

    Actualizacion de todo.service.ts

--- src/todo/services/todo.service.ts
+++ src/todo/services/todo.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Todo } from '../schemas/todo.schema';
 import { Model } from 'mongoose';
 import { CreateTodoDTO } from '../dto/create-todo.dto';
+import { UpdateTodoDTO } from '../dto/update-todo.dto';
 
 @Injectable()
 export class TodoService {
@@ -12,5 +13,25 @@ export class TodoService {
     const createdTodo = new this.todoModel(todo);
 
     return createdTodo.save();
+  }
+
+  public async findAll() {
+    return this.todoModel.find().exec();
+  }
+
+  public async findOne(id: string) {
+    return this.todoModel.findById(id).exec();
+  }
+
+  public async update(id: string, todo: UpdateTodoDTO) {
+    return this.todoModel
+      .findByIdAndUpdate(id, todo, {
+        new: true,
+      })
+      .exec();
+  }
+
+  public async delete(id: string) {
+    return this.todoModel.findByIdAndDelete(id).exec();
   }
 }

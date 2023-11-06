b624c86e2d29f21887374bf11b5c2d5924002a7c
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-05 23:02:37 -0400

    Modificacion de app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://manuelfbs:321Shalom321@cluster0.irhtnet.mongodb.net/nestjsdb?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.irhtnet.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`,
     ),
     TodoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+// 'mongodb+srv://manuelfbs:321Shalom321@cluster0.irhtnet.mongodb.net/nestjsdb?retryWrites=true&w=majority'
+//
 export class AppModule {}

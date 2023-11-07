2da64e04b1accf829cda5d7138e1b573e64daed5
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-07 00:23:37 -0400

    switching a BD railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,8 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.irhtnet.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`,
+      //`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.irhtnet.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`,
+      `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.proxy.rlwy.net:46974/${process.env.MONGODB_DBNAME}?authSource=admin`,
     ),
     TodoModule,
   ],
